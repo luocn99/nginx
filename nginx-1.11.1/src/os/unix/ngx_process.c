d8f433d828828edc66498378b7409fcb17a95cde
luocn99 <luocn99@gmail.com>
2016-06-20 20:21:10 +0800

    add

--- nginx-1.11.1/src/os/unix/ngx_process.c
+++ nginx-1.11.1/src/os/unix/ngx_process.c
@@ -362,7 +362,7 @@ ngx_signal_handler(int signo)
             break;
 
         //case ngx_signal_value(NGX_SRECONFIGURE_SIGNAL):
-        case SIGRTMIN + 10:
+        //case SIGRTMIN + 10:
             ngx_stgw_dyn_reconfigure = 1;
             action = ", stgw dyn reconfiguring";
             break;
